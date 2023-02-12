5e7c1f377141e2e6a49f1d8efac2a576753ce7bf
Weijia <zhengwj.william@gmail.com>
2023-02-12 21:30:32 +0800

    1st A-Channel

---
+++ a_channel_utils.py
@@ -0,0 +1,26 @@
+import random
+import numpy as np
+from scipy.stats import bernoulli
+
+
+def ach_binary_to_symbol(txBitsParitized, L, K, J):
+    tx_symbols = np.zeros((K,L), dtype=int)
+    for l in range(L):
+        tx_symbols[:,l] = txBitsParitized[:,l*J:(l+1)*J] @ 2**np.arange(J)[::-1].reshape(-1)
+    
+    # print(f'info_symbols.shape: {tx_symbols.shape}')
+    # print(tx_symbols[0:5, :])
+    return tx_symbols
+
+
+def ach_with_error(tx_symbols, L, K, J, p_e):
+    for l in range(L):
+        applyErrs = np.where(bernoulli.rvs(p_e, size=K))[0]
+        Errs = np.random.randint(2**J, size=len(applyErrs))
+        tx_symbols[applyErrs,l] = Errs
+    
+    rng = np.random.default_rng()
+    tx_symbols = rng.permuted(tx_symbols, axis=0)
+    # print(f'After A-Channel, info_symbols.shape: {tx_symbols.shape}')
+    # print(tx_symbols[0:5, :])
+    return tx_symbols
--- demo.py
+++ demo.py
@@ -1,12 +1,21 @@
-from joblib import Parallel, delayed
-import numpy as np
+# from joblib import Parallel, delayed
+# import numpy as np
 
-def process(i):
-    return np.array([i * i, 1]).reshape(1,-1)
+# def process(i):
+#     return np.array([i * i, 1]).reshape(1,-1)
     
-results = Parallel(n_jobs=-1)(delayed(process)(i) for i in range(10))
-print(np.array(results).reshape(-1,2))  # prints [0, 1, 4, 9, 16, 25, 36, 49, 64, 81]
+# results = Parallel(n_jobs=-1)(delayed(process)(i) for i in range(10))
+# print(np.array(results).reshape(-1,2))  # prints [0, 1, 4, 9, 16, 25, 36, 49, 64, 81]
 
-for result in results:
-    print(sum(result).all())
-    print(result.all() != None)
+# for result in results:
+#     print(sum(result).all())
+#     print(result.all() != None)
+
+
+import numpy as np
+import scipy
+from scipy.stats import bernoulli
+
+p = 0.1
+r = bernoulli.rvs(p, size=1000)
+print(r)
