ef2dba5b2f73ff0db690d4b885d7c1858f6c4e5d
Weijia <zhengwj.william@gmail.com>
2023-02-20 01:23:01 +0800

    plotted the results in graph

--- demo.py
+++ demo.py
@@ -24,32 +24,74 @@
 
 
 
-# import numpy as np
-# import matplotlib.pyplot as pyplot
-
-# phase1_true = np.array([109, 125])
-# phase1_false= np.array([0,  0])
-# phase2_true = np.array([26, 15])
-# phase2_false= np.array([4,  5])
-# p_es = np.array(["0.02", "0.01"])
-# pyplot.axhline(y = 150, color = 'black', linestyle = 'dashed')
-# pyplot.bar(p_es, phase1_true)
-# pyplot.bar(p_es, phase2_true, bottom = phase1_true)
-# pyplot.bar(p_es, phase1_false, bottom = phase1_true + phase2_true)
-# pyplot.bar(p_es, phase2_false, bottom = phase1_true+phase1_false+phase2_true)
-
-
-# pyplot.legend([ "K=150","Ph.1 True", "Ph.2 True","Ph.1 False",  "Ph.2 False"])
-# pyplot.xlabel("p_e")
-# pyplot.ylim([0,160])
-# pyplot.title("K= 150")
-# pyplot.show()
-
-
-
 import numpy as np
-from scipy.stats import bernoulli
+import matplotlib.pyplot as pyplot
 
-np.random.seed(2)
-applyErrs = np.where(bernoulli.rvs(0.1, size=100))[0]
-print(applyErrs)
+
+phase1_llc_true = np.array([15, 37, 63])
+phase1_llc_false= np.array([0,  0,  0])
+phase2_llc_true = np.array([31, 39, 31])
+phase2_llc_false= np.array([0,  3,  0])
+phase1_tc_true = np.array([14, 37, 63])
+phase1_tc_false= np.array([1,  1,  3])
+p_es = np.array(["0.1", "0.05", "0.025"])
+x_axis = np.arange(len(p_es))
+
+pyplot.axhline(y = 100, color = 'black', linestyle = 'dashed')
+pyplot.bar(x_axis-0.15, phase1_llc_true,  0.2)
+pyplot.bar(x_axis-0.15, phase2_llc_true,  0.2, bottom = phase1_llc_true)
+pyplot.bar(x_axis-0.15, phase1_llc_false, 0.2, bottom = phase1_llc_true + phase2_llc_true)
+pyplot.bar(x_axis-0.15, phase2_llc_false, 0.2, bottom = phase1_llc_true + phase2_llc_true + phase1_llc_false)
+
+pyplot.bar(x_axis+0.15, phase1_tc_true,  0.2)
+pyplot.bar(x_axis+0.15, phase1_tc_false, 0.2,  bottom = phase1_tc_true)
+
+pyplot.xticks(x_axis, p_es)
+pyplot.legend([ "K=100","Ph.1 True (LLC)", "Ph.2 True (LLC)","Ph.1 False (LLC)",  "Ph.2 False (LLC)", "Ph.1 True (TreeCode)", "Ph.1 False (TreeCode)"])
+pyplot.xlabel("p_e")
+pyplot.ylim([0,110])
+pyplot.title("K= 100 Linked-loop code and Tree code")
+pyplot.show()
+
+
+
+phase1_llc_true = np.array([28, 65, 96])
+phase1_llc_false= np.array([0,  0,  0])
+phase2_llc_true = np.array([48, 49, 43])
+phase2_llc_false= np.array([4,  8,  2])
+phase1_tc_true  = np.array([28, 65, 96])
+phase1_tc_false = np.array([3,  8,  6])
+p_es = np.array(["0.1", "0.05", "0.025"])
+x_axis = np.arange(len(p_es))
+
+pyplot.axhline(y = 150, color = 'black', linestyle = 'dashed')
+pyplot.bar(x_axis-0.15, phase1_llc_true,  0.2)
+pyplot.bar(x_axis-0.15, phase2_llc_true,  0.2, bottom = phase1_llc_true)
+pyplot.bar(x_axis-0.15, phase1_llc_false, 0.2, bottom = phase1_llc_true + phase2_llc_true)
+pyplot.bar(x_axis-0.15, phase2_llc_false, 0.2, bottom = phase1_llc_true + phase2_llc_true + phase1_llc_false)
+
+pyplot.bar(x_axis+0.15, phase1_tc_true,  0.2)
+pyplot.bar(x_axis+0.15, phase1_tc_false, 0.2,  bottom = phase1_tc_true)
+
+pyplot.xticks(x_axis, p_es)
+pyplot.legend([ "K=150","Ph.1 True (LLC)", "Ph.2 True (LLC)","Ph.1 False (LLC)",  "Ph.2 False (LLC)", "Ph.1 True (TreeCode)", "Ph.1 False (TreeCode)"])
+pyplot.xlabel("p_e")
+pyplot.ylim([0,160])
+pyplot.title("K= 150 Linked-loop code and Tree code")
+pyplot.show()
+
+
+
+
+
+
+
+
+
+
+# import numpy as np
+# from scipy.stats import bernoulli
+
+# np.random.seed(2)
+# applyErrs = np.where(bernoulli.rvs(0.1, size=100))[0]
+# print(applyErrs)
